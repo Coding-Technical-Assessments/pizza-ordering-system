0bee4bdb60d80f10cc2f253b758e7e66f4b2b28f
ProgrammerJayT <theanthem8@gmail.com>
2024-09-06 12:46:49 +0200

    Wrap view with auth user layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,17 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
+import UserLayout from "@/layout/user";
+import { Box } from "@mui/material";
+import { AppProvider } from "@/context/app";
+import FeedbackComponents from "@/components/feedback";
+import { initializeAxios } from "@/config/axios";
+import { AuthProvider } from "@/context/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +23,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <UserLayout>{children}</UserLayout>
+      </body>
     </html>
   );
 }
