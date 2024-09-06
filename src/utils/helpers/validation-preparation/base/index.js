58db328fa333fae4a151fdf266212c68ec264b12
ProgrammerJayT <theanthem8@gmail.com>
2024-09-06 12:53:12 +0200

    Fix method name

--- src/utils/helpers/validation-preparation/base/index.js
+++ src/utils/helpers/validation-preparation/base/index.js
@@ -1,11 +1,11 @@
-export const formatSizeCreateData = (data) => {
+export const formatBaseCreateData = (data) => {
   const newObj = {};
 
   Object.entries(data).forEach(([key, value]) => {
     if (typeof value === "string") {
       newObj[key] = value.toLowerCase();
     } else if (typeof value === "object" && value !== null) {
-      newObj[key] = formatSizeCreateData(value);
+      newObj[key] = formatBaseCreateData(value);
     } else {
       newObj[key] = value;
     }
